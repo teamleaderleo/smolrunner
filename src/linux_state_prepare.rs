01c09396c20a1795982dc961e6d136d0c6e811bb
Leo <leoli_082000@hotmail.com>
2026-07-24 05:39:10 +0800

    Use rustfmt ownership predicate layout

--- src/linux_state_prepare.rs
+++ src/linux_state_prepare.rs
@@ -151,9 +151,7 @@ fn inspect_managed_directory(
             format!("{subject} does not have mode 0750"),
         ));
     }
-    if expected_owner
-        .is_some_and(|(uid, gid)| stat.st_uid != uid || stat.st_gid != gid)
-    {
+    if expected_owner.is_some_and(|(uid, gid)| stat.st_uid != uid || stat.st_gid != gid) {
         return Err(StateStoreError::public(
             StateStoreErrorKind::UnsafeFilesystem,
             format!("{subject} has an unexpected owner or group"),
